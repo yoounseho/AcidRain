0e78f1d729945fd7e95ea08828a6bb23b84f1f96
seho-y <dbstpghdbs@gmail.com>
2019-12-06 11:15:07 +0900

    프로토제작
    단순 기능구현

---
+++ AcidRain/Source/AcidRain/Public/World/AcidRainGameInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "AcidRainGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ACIDRAIN_API UAcidRainGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
